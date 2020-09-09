29db4f4d510a221de18d9e9fd3d85f4e004f9396
ShirinZafarmand <70761293+ShirinZafarmand@users.noreply.github.com>
2020-09-10 02:18:36 +0430

    Practiced JavaScript Console!

--- Drawing(Session 2)/template-p5-project/js/script.js
+++ Drawing(Session 2)/template-p5-project/js/script.js
@@ -9,8 +9,14 @@ Here is a description of this template p5 project.
 //
 // Description of setup() goes here.
 function setup() {
-  background(200,200,200);
 
+  //Expanded Canvas
+createCanvas(700,700);
+  //set the background color to Grey
+background(200,200,200);
+//Created a cross
+line(20,20,80,40);
+line(80,20,20,40);
 }
 
 // draw()
