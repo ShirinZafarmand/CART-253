262e20ef3a59a1b01cb11a85ff1da030a8559e73
ShirinZafarmand <70761293+ShirinZafarmand@users.noreply.github.com>
2020-09-10 01:49:18 +0430

    Started drawing!

---
+++ Drawing(Session 2)/template-p5-project/js/script.js
@@ -0,0 +1,21 @@
+/**************************************************
+Template p5 project
+Pippin Barr
+
+Here is a description of this template p5 project.
+**************************************************/
+
+// setup()
+//
+// Description of setup() goes here.
+function setup() {
+  background(200,200,200);
+
+}
+
+// draw()
+//
+// Description of draw() goes here.
+function draw() {
+
+}
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# CART-253
+# CART-253 - Creative Computation 1
 Works I get to do for CART 253 :)
+
+Everything Is about Design!
