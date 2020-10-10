2f4fe85025fdc6e6b33633d7b21a4faf84351c1e
ShirinZafarmand <70761293+ShirinZafarmand@users.noreply.github.com>
2020-10-10 16:49:32 +0330

    Project 1) added some expandable rectangles for user to avoid!

--- PROJECTS/Project 1/js/script.js
+++ PROJECTS/Project 1/js/script.js
@@ -7,9 +7,9 @@ Shirin Zafarmand
 Here is a description of this template p5 project.
 **************************************************/
 let bg={
-  r:0,
-  g:0,
-  b:0
+  r:51,
+  g:50,
+  b:49
 };
 
 let border={
@@ -43,21 +43,26 @@ let troublesome1={
 };
 
 let troublesome2={
-  size1:100,
+  size1:110,
   size2:0,
   expansion:5,
 };
 
 let troublesome3={
   size1:0,
+  size2:110,
+  expansion:5,
+};
+
+let troublesome4={
+  size1:0,
   size2:100,
   expansion:5,
 };
 
-
 function setup() {
   createCanvas(windowWidth,windowHeight);
-  noStroke();
+  noStroke()
 };
 
 
@@ -66,7 +71,59 @@ function draw() {
   background(bg.r,bg.g,bg.b);
 
 
+
+//troublesome 1
+  fill(255)
+  rect(windowWidth/4,0,troublesome1.size1,troublesome1.size2);
+  troublesome1.size2 = troublesome1.size2 + troublesome1.expansion;
+  if (troublesome1.size2>windowHeight/2){
+    troublesome1.expansion= -troublesome1.expansion
+  };
+  if (troublesome1.size2<2){
+    troublesome1.expansion= -troublesome1.expansion
+  };
+
+//troublesome 4
+  rect(0,2*windowHeight/3,troublesome4.size1,troublesome4.size2);
+  troublesome4.size1 = troublesome4.size1 + troublesome4.expansion;
+  if (troublesome4.size1>windowWidth/4){
+    troublesome4.expansion= -troublesome4.expansion
+  };
+  if (troublesome4.size1<2){
+    troublesome4.expansion= -troublesome4.expansion
+  };
+
+  rect(3*windowWidth/4,windowHeight/3,windowWidth/4,100);
+  rect(3*windowWidth/4,windowHeight/2,100,windowHeight/2);
+
+
+//troublesome 2
+push()
+  fill(bg.r,bg.g,bg.b)
+  stroke(bg.r,bg.g,bg.b)
+  strokeWeight(50)
+  rect(3*windowWidth/4,windowHeight/2,troublesome2.size1,troublesome2.size2);
+  troublesome2.size2 = troublesome2.size2 + troublesome2.expansion;
+  if (troublesome2.size2>windowHeight/2){
+    troublesome2.expansion= -troublesome2.expansion
+  };
+  if (troublesome2.size2<2){
+    troublesome2.expansion= -troublesome2.expansion
+  };
+
+//troublesome 3
+  rect(3*windowWidth/4,windowHeight/3,troublesome3.size1,troublesome3.size2);
+  troublesome3.size1 = troublesome3.size1 + troublesome3.expansion;
+  if (troublesome3.size1>windowWidth/4){
+    troublesome3.expansion= -troublesome3.expansion
+  };
+  if (troublesome3.size1<2){
+    troublesome3.expansion= -troublesome3.expansion
+  };
+pop()
+
 //user
+  fill(255, 229, 181)
   ellipse(user.x,user.y,user.size);
   if (keyIsDown(37)){
       user.x= user.x - 10;
@@ -80,8 +137,6 @@ function draw() {
     else if(keyIsDown(40)){
       user.y=user.y + 10
     };
-
-
 
 //horizantal borders
   for(let i = 0; i < 25; i++){
