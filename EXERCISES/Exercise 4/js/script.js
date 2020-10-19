a50aa8ddd0bd590ac2674365248ef02424dd0553
ShirinZafarmand <70761293+ShirinZafarmand@users.noreply.github.com>
2020-10-19 22:24:42 +0330

    Exercise 4) added queen bee

--- EXERCISES/Exercise 4/js/script.js
+++ EXERCISES/Exercise 4/js/script.js
@@ -1,7 +1,7 @@
 "use strict";
 
 let beeSquad=[];
-let squadSize=250;
+let squadSize=170;
 
 let bg={
   r:0,
@@ -9,15 +9,37 @@ let bg={
   b:0
 };
 
+let queenBee={
+  fill:{
+    r:92,
+    g:212,
+    b:76,
+  },
+  x:0,
+  y:0,
+  size:100,
+  vx:0,
+  vy:0,
+  speed:2
+};
+
+let user={
+  x:0,
+  y:0,
+  size:50,
+};
 
 
 function setup() {
-createCanvas(windowWidth,windowHeight);
-noStroke();
-for (let i = 0; i < squadSize; i++){
-  beeSquad[i]= createBee(random(0,width),random(0,height));
-}
-}
+  createCanvas(windowWidth,windowHeight);
+  noStroke();
+  for (let i = 0; i < squadSize; i++){
+    beeSquad[i]= createBee(random(0,width),random(0,height));
+  };
+  queenBee.x=random(0,width);
+  queenBee.y=random(0,height);
+};
+
 
 function createBee(x,y){
   let bee={
@@ -29,22 +51,27 @@ function createBee(x,y){
     speed:5,
   };
   return bee;
-}
-
+};
 
 
 function draw() {
   background(bg.r,bg.g,bg.b);
+
   for (let i = 0; i<beeSquad.length; i++){
-    moveBee(beeSquad[i])
-  }
-
-
-
+    moveBee(beeSquad[i]);
+  };
   for (let i=0; i<beeSquad.length; i++){
-      displayBee(beeSquad[i]);
-  }
-}
+    displayBee(beeSquad[i]);
+  };
+
+  displayQueenBee();
+
+  moveQueenBee();
+
+  displayUser();
+
+  beeUserEncounter();
+};
 
 
 
@@ -56,17 +83,59 @@ function moveBee(bee){
     bee.vy=random(-bee.speed,bee.speed);
   }
 
-bee.x=bee.x+bee.vx;
-bee.y=bee.y+bee.vy;
+  bee.x=bee.x+bee.vx;
+  bee.y=bee.y+bee.vy;
 
-bee.x=constrain(bee.x,0,width);
-bee.y=constrain(bee.y,0,height)
-}
+  bee.x=constrain(bee.x,0,width);
+  bee.y=constrain(bee.y,0,height);
+};
 
 
 function displayBee(bee){
-  push()
-  fill(200,0,10)
-  ellipse(bee.x,bee.y,bee.size)
-  pop()
-}
+  push();
+  fill(235, 180, 52);
+  ellipse(bee.x,bee.y,bee.size);
+  pop();
+};
+
+
+function moveQueenBee(){
+  let change=random(0,20)
+  if (change<0.5){
+    queenBee.vx=random(-queenBee.speed,queenBee.speed);
+    queenBee.vy=random(-queenBee.speed,queenBee.speed);
+  };
+
+  queenBee.x=queenBee.x+queenBee.vx;
+  queenBee.y=queenBee.y+queenBee.vy;
+
+  queenBee.x=constrain(queenBee.x,0,width);
+  queenBee.y=constrain(queenBee.y,0,height);
+};
+
+
+
+function displayQueenBee(){
+  push();
+  fill(96, 114, 230);
+  ellipse(queenBee.x,queenBee.y,queenBee.size);
+  pop();
+};
+
+
+function displayUser(){
+  push();
+  fill(230, 112, 96);
+  user.x=mouseX;
+  user.y=mouseY;
+  ellipse(user.x,user.y,user.size);
+  pop();
+};
+
+
+function beeUserEncounter(){
+  let d =dist(user.x,user.y,bee.x,bee.y)
+  if(d<user.size/2+bee.size/2){
+    noLoop();
+  };
+};
