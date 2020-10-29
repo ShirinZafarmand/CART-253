14135d2efeb56735812b6c96cd9e904c6a76d578
ShirinZafarmand <70761293+ShirinZafarmand@users.noreply.github.com>
2020-10-29 13:25:24 +0330

    Project 2) started working on my astronaut movements!

--- PROJECTS/Project 2/js/script.js
+++ PROJECTS/Project 2/js/script.js
@@ -1,22 +1,45 @@
 "use strict";
 
 /**************************************************
-Template p5 project
-Pippin Barr
+Project 2-prototype stage
+Shirin Zafarmand
 
 Here is a description of this template p5 project.
 **************************************************/
+let bg={
+  r:24,
+  g:18,
+  b:110
+};
 
-// setup()
-//
-// Description of setup() goes here.
+let astronaut={
+    x:200,
+    y:250,
+    width:500,
+    height:300,
+    vx:0,
+    vy:0,
+    ax:0,
+    ay:0,
+    speed:0.4,
+    fill:{ r:0, g:0, b:0},
+    image:undefined
+  };
+
+
+function preload(){
+  astronaut.image=loadImage("assets/images/astronaut.png")}
+
+
 function setup() {
-
+  createCanvas(windowWidth,windowHeight);
 }
 
-// draw()
-//
-// Description of draw() goes here.
-function draw() {
 
+function draw() {
+  background(bg.r,bg.g,bg.b)
+
+
+  imageMode(CENTER)
+  image(astronaut.image,astronaut.x,astronaut.y,astronaut.width,astronaut.height)
 }
