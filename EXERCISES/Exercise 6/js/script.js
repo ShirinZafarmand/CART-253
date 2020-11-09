a430323acfb2213e4ec4cf64dcfde994730e5718
ShirinZafarmand <70761293+ShirinZafarmand@users.noreply.github.com>
2020-11-09 10:15:07 -0500

    Exercise 6) started!

---
+++ EXERCISES/Exercise 6/js/script.js
@@ -0,0 +1,70 @@
+"use strict";
+
+/**************************************************
+Template p5 project
+Pippin Barr
+
+Here is a description of this template p5 project.
+**************************************************/
+let astronaut={
+  x:1200,
+  y:{
+    max:650,
+    min:450,
+    normal:550
+  },
+  width:500,
+  height:300,
+  vx:0,
+  vy:0,
+  ax:0,
+  ay:0,
+  speed:1,
+  fill:{ r:0, g:0, b:0},
+  image:undefined,
+  hover: true,
+};
+
+let moon={
+  x:1500,
+  y:1300,
+  width:3100,
+  height:1500,
+  image:undefined,
+};
+
+function preload(){
+  astronaut.image=loadImage("assets/images/astronaut.png");
+  moon.image=loadImage("assets/images/moon2.png");
+};
+
+function setup() {
+  createCanvas(windowWidth,windowHeight);
+}
+
+
+function draw() {
+    background(bg.r,bg.g,bg.b);
+
+    //moon displaying
+    image(moon.image,moon.x,moon.y,moon.width,moon.height)
+
+    //astronaut display
+    imageMode(CENTER)
+    image(astronaut.image,astronaut.x,astronaut.y.normal,astronaut.width,astronaut.height)
+
+
+    //astronaut hover mode
+    astronaut.y.normal= astronaut.y.normal+ astronaut.speed
+
+    if (astronaut.hover= true &&
+      astronaut.y.normal>=astronaut.y.max){
+        astronaut.speed=-astronaut.speed
+      }
+
+      if (astronaut.hover= true &&
+        astronaut.y.normal<=astronaut.y.min){
+          astronaut.speed=-astronaut.speed
+        }
+        
+}
