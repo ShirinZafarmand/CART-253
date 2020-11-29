0803cd08a269363a8f879ed6c778fa45e1dcb0f0
ShirinZafarmand <70761293+ShirinZafarmand@users.noreply.github.com>
2020-11-29 17:20:11 -0500

    Project 2) displaying arrow

--- PROJECTS/Stage 3/js/aliens.js
+++ PROJECTS/Stage 3/js/aliens.js
@@ -2,7 +2,7 @@ class Aliens{
 
   constructor(){
     //Aliens
-    this.alien1={
+    this.alien5={
       x:500,
       y:50,
       size:70,
@@ -12,7 +12,7 @@ class Aliens{
       angle:0
     };
 
-    this.alien2={
+    this.alien4={
       x:600,
       y:50,
       size:70,
@@ -32,7 +32,7 @@ class Aliens{
       angle:0,
     };
 
-    this.alien4={
+    this.alien2={
       x:680,
       y:50,
       size:70,
@@ -42,7 +42,7 @@ class Aliens{
       angle:0,
     };
 
-    this.alien5={
+    this.alien1={
       x:730,
       y:50,
       size:70,
@@ -51,7 +51,7 @@ class Aliens{
       b:65,
       angle:0,
     };
-}
+  }
 
 
   displayAlien1(){
@@ -63,9 +63,11 @@ class Aliens{
   };
 
   displayAlien2(){
-    rotate(this.alien2.angle);
-    rect(this.alien2.x,this.alien2.y,this.alien2.size,this.alien2.size);
-    this.alien2.angle=this.alien2.angle+0.01/3;
+    if(this.alien1.angle>=3){
+      rotate(this.alien2.angle);
+      rect(this.alien2.x,this.alien2.y,this.alien2.size,this.alien2.size);
+      this.alien2.angle=this.alien2.angle+0.01/3;
+    }
   };
 
   displayAlien3(){
--- PROJECTS/Stage 3/js/script.js
+++ PROJECTS/Stage 3/js/script.js
@@ -24,14 +24,26 @@ let moon={
 };
 
 let astronaut={
-  x:600,
-  y:400,
+  x:1490,
+  y:590,
   width:500,
   height:300,
   fill:{ r:0, g:0, b:0},
   image:undefined,
   shrink:0.05,
 }
+
+let arrow={
+  x:0,
+  y:0,
+  width:10,
+  height:80,
+  angle:0,
+  rotation:0,
+  fill:255,
+  expansion:0,
+  reduction:0
+};
 
 function preload(){
   moon.image=loadImage("assets/images/moon2.png");
@@ -53,17 +65,23 @@ function setup() {
 function draw() {
   background(bg.r,bg.g,bg.b);
 
+  // making the moon surface slowly disapear from the screen
+  moon.y = moon.y + moon.movement;
+
   //displaying moon
   image(moon.image,moon.x,moon.y,moon.width,moon.height);
 
   // Draw the astronaut
   image(astronaut.image,astronaut.x,astronaut.y,astronaut.width,astronaut.height);
 
-  //absorbing the astronaut to the senter of the screen
-  let dx = dist(astronaut.x,astronaut.y,width/2,height/2)
-  let horizentalMovement= dx/100
-  astronaut.x=astronaut.x+dx/100
+  //arrow
+  arrowDisplay();
 
+  //aiming a lover
+  aimingArrow();
+
+  //shooting the arrow
+  shootingArrow();
 
   //displaying Aliens
   push();
@@ -73,5 +91,35 @@ function draw() {
   alien.displayAlien4();
   alien.displayAlien5();
   pop();
-
 }
+
+function aimingArrow(){
+  if (keyIsDown(37)){
+    arrow.rotation=0.1;
+    arrow.angle=arrow.angle+arrow.rotation}
+    else if(keyIsDown(39)){
+      arrow.rotation=-0.1;
+      arrow.angle=arrow.angle+arrow.rotation}
+    };
+
+function arrowDisplay(){
+  push();
+  translate(1510,640);
+  rotate(arrow.angle);
+  fill(255);
+  rect(arrow.x,arrow.y,arrow.width,arrow.height);
+  pop();
+};
+
+function shootingArrow(){
+  arrow.height=arrow.height+arrow.expansion;
+  if(arrow.height>1000){
+  noLoop();
+ }
+};
+
+//shoot the arrow
+function keyPressed(){
+  if (keyCode===38){
+    arrow.expansion=750}
+  };
